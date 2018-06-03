09b1e4396d97bba2a83559d5af496415c42d3296
Yuusaku Hayashi <forestmtree@gmail.com>
2018-06-03 20:24:59 +0900

    lesson16 add

---
+++ lesson15.rb
@@ -0,0 +1,17 @@
+# While
+
+i = 0
+
+while i < 10 do
+	puts "#{i}times hello"
+	i += 1
+end
+
+# Times
+#
+10.times do
+	puts "hello" 
+end
+
+10.times{puts "hello"}
+
---
+++ lesson16.rb
@@ -0,0 +1,14 @@
+# for
+#
+for i in 15..20 do
+	p i
+end
+
+for color in ["red", "blue"] do
+	p color 
+end
+
+for name, score in {taguchi:200, fkoji:400} do
+	puts "#{name}: #{score}" 
+end
+
