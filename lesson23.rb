11fb6220db98ab1689bcd6a887191c7a5a38a234
Yuusaku Hayashi <forestmtree@gmail.com>
2018-06-05 23:07:09 +0900

    lesson23 add

---
+++ lesson23.rb
@@ -0,0 +1,25 @@
+#Public, Private
+#
+class User
+	def initialize(name = "ruby")
+		@name = name
+	end
+
+	def sayPrivateGreeting
+		sayGreeting
+	end
+
+	private
+		def sayGreeting
+			puts %(hello my name is #{@name})
+		end
+end
+
+class AdminUser < User
+end
+
+user = User.new
+user.sayPrivateGreeting
+
+admin = AdminUser.new
+admin.sayGreeting
