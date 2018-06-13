852ad9037171b2710ac800dcdbd1d039851766ab
Yuusaku Hayashi <forestmtree@gmail.com>
2018-06-13 22:26:03 +0900

    lesson26 add

---
+++ lesson26.rb
@@ -0,0 +1,19 @@
+class MyError < StandardError; end
+
+x = gets.to_i
+
+begin
+	if x == 3
+		raise MyError
+	end
+	p 100 / x
+
+rescue MyError
+	puts "no 3!"
+rescue => ex
+	p ex.message
+	p ex.class
+	puts "stopped!"
+ensure
+	puts "-- END --"
+end
